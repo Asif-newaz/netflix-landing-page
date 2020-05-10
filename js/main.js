16d798a7901a26b84679798c92a9ce3964df2c9a
Asif-newaz <max.asif24@gmail.com>
2020-05-10 16:04:07 +0600

    JS and HTML file has been newly formatted

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ const tabItems = document.querySelectorAll('.tab-item');
 const tabContentItems = document.querySelectorAll('.tab-content-item');
 
 //select tab content item
-function selectItem(e){
+function selectItem(e) {
     removeBorder();
     removeShow();
     //Add border to current tab
     this.classList.add('tab-border');
-    
+
     //Grab item from DOM
     const tabContentItem = document.getElementById(`${this.id}-content`);
     //Add show class
@@ -15,11 +15,11 @@ function selectItem(e){
 
 }
 
-function removeBorder(){
+function removeBorder() {
     tabItems.forEach(item => item.classList.remove('tab-border'));
 }
 
-function removeShow(){
+function removeShow() {
     tabContentItems.forEach(item => item.classList.remove('show'));
 }
 
